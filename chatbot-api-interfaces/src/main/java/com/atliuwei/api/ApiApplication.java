bd2ed447d431b6d5736b55640bf9efbcff8eefc6
liuwei <liuwei1098122943@gmail.com>
2023-03-07 17:41:20 +0800

    liuwei,feat:init ddd project

---
+++ chatbot-api-interfaces/src/main/java/com/atliuwei/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.atliuwei.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuwei
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/7 17:27
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/atliuwei/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package com.atliuwei.chatbot.api.test;
+
+/**
+ * @author liuwei
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/7 17:36
+ */
+public class ApiTest {
+}
