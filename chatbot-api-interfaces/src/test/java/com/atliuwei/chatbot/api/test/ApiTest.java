9425930d4b8f3b085359dae51180fbb18d52d6d6
liuwei <liuwei1098122943@gmail.com>
2023-03-11 16:22:37 +0800

    liuwei,feat:对接chatGPT 接口

--- chatbot-api-interfaces/src/test/java/com/atliuwei/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/com/atliuwei/chatbot/api/test/ApiTest.java
@@ -65,5 +65,29 @@ public class ApiTest {
         }
     }
 
+    @Test
+    //给ChatGTP发送请求
+    public void test_chatGPT() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.openai.com/v1/completions");
+        post.addHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Bearer 自行申请 https://beta.openai.com/overview");
+
+        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"帮我写一个java冒泡排序\", \"temperature\": 0, \"max_tokens\": 1024}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
+
 
 }
